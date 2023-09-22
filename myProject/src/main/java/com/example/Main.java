c5b5aa59e0f88e3c5d4238d9d7d809787618129d
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-09-22 22:50:35 +0500

    Add files via upload

---
+++ myProject/src/main/java/com/example/Main.java
@@ -0,0 +1,23 @@
+package main.java.com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        // Создаем экземпляр класса Calculator
+        Calculator calculator = new Calculator();
+
+        // Входные данные: сумма покупки и процент скидки
+        double purchaseAmount = 100.0;
+        int discountAmount = 20;
+
+        try {
+            // Вычисляем сумму с учетом скидки
+            double discountedAmount = calculator.calculateDiscount(purchaseAmount, discountAmount);
+
+            // Выводим результат
+            System.out.println("Сумма покупки со скидкой: " + discountedAmount);
+        } catch (ArithmeticException e) {
+            // Если произошла ошибка (например, недопустимые аргументы), выводим сообщение
+            System.err.println("Ошибка: " + e.getMessage());
+        }
+    }
+}
