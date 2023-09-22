803d0ffc65e3cba23d67e917b68b566d8c2e1ce9
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-09-22 22:49:32 +0500

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,11 @@
+public class Calculator {
+
+    public static double calculateDiscount(double purchaseAmount, int discountAmount) {
+        if (purchaseAmount < 0 || discountAmount < 0 || discountAmount > 100) {
+            throw new ArithmeticException("Invalid input: purchaseAmount or discountAmount is out of range.");
+        }
+
+        double discount = purchaseAmount * discountAmount / 100.0;
+        return purchaseAmount - discount;
+    }
+}
