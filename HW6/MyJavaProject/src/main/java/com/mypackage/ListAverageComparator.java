7ba2d8935af95c68457cbc8c2e51d08a4799f54d
AbubakirovRA <91825137+AbubakirovRA@users.noreply.github.com>
2023-10-03 23:13:59 +0500

    Add files via upload

---
+++ HW6/MyJavaProject/src/main/java/com/mypackage/ListAverageComparator.java
@@ -0,0 +1,42 @@
+import java.util.List;
+
+public class ListAverageComparator {
+    /**
+     * Сравнивает средние значения двух списков.
+     *
+     * @param list1 Первый список для сравнения.
+     * @param list2 Второй список для сравнения.
+     * @return Возвращает отрицательное значение,
+     *         если среднее значение list1 меньше среднего значения list2,
+     *         положительное значение, если среднее значение list1 больше
+     *         среднего значения list2, и ноль, если средние значения равны.
+     */
+
+    public String compareAverages(final List<Integer> list1,
+    final List<Integer> list2) {
+        double average1 = calculateAverage(list1);
+        double average2 = calculateAverage(list2);
+
+        if (average1 > average2) {
+            return "Первый список имеет большее среднее значение";
+        } else if (average1 < average2) {
+            return "Второй список имеет большее среднее значение";
+        } else {
+            return "Средние значения равны";
+        }
+    }
+
+    private double calculateAverage(final List<Integer> list) {
+        if (list.isEmpty()) {
+            return 0;
+        }
+
+        double sum = 0;
+        for (int num : list) {
+            sum += num;
+        }
+        return sum / list.size();
+    }
+}
+
+
---
+++ HW6/MyJavaProject/src/test/java/com/mypackage/ListAverageComparatorTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Test;
+import java.util.Arrays;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+//import static org.junit.jupiter.api.Assertions.*;
+
+public class ListAverageComparatorTest {
+    @Test
+    public void testCompareAveragesFirstListGreater() {
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = Arrays.asList(1, 2);
+
+        ListAverageComparator comparator = new ListAverageComparator();
+        String result = comparator.compareAverages(list1, list2);
+
+        assertEquals("Первый список имеет большее среднее значение", result);
+    }
+
+    @Test
+    public void testCompareAveragesSecondListGreater() {
+        List<Integer> list1 = Arrays.asList(1, 2);
+        List<Integer> list2 = Arrays.asList(1, 2, 3);
+
+        ListAverageComparator comparator = new ListAverageComparator();
+        String result = comparator.compareAverages(list1, list2);
+
+        assertEquals("Второй список имеет большее среднее значение", result);
+    }
+
+    @Test
+    public void testCompareAveragesEqualAverages() {
+        List<Integer> list1 = Arrays.asList(1, 2, 3);
+        List<Integer> list2 = Arrays.asList(1, 2, 3);
+
+        ListAverageComparator comparator = new ListAverageComparator();
+        String result = comparator.compareAverages(list1, list2);
+
+        assertEquals("Средние значения равны", result);
+    }
+}
